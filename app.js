65fd72562ad1e7f8e892277c82a6c8a1107cf790
zainab <zainab@gmail.com>
2022-07-22 22:56:12 +0300

    styling

--- app.js
+++ app.js
@@ -1,103 +1,118 @@
-let task=document.getElementById("task")
-let button_task=document.getElementById("button_task")
-let tasks=document.getElementById("tasks")
-let listtask=document.querySelector("ul")
-console.log(listtask);
+
+let button_task = document.getElementById("button_task")
+let listtask = document.querySelector(".tasks")
+let notasks_item = document.querySelector('.no-tasks')
+let input=document.querySelector("#input")
 
 
-let taskscontanier=document.querySelector(".taskscontanier")
-let removetask=document.getElementById("remove")
 
-let add_task=() => {
+
+let taskscontanier = document.querySelector(".taskscontanier")
+let removetask = document.getElementById("remove")
+
+let add_task = () => {
 
     // removetask.style.display="none"
-    button_task.addEventListener("click",() => {
-        let newitem=document.createElement('li')
-        let paragah=document.createElement('span')
-        paragah.innerHTML=task.value
-        if(paragah.innerHTML==''){
+    button_task.addEventListener("click", () => {
+
+        notasks_item.remove()
+        notasks_item.classList.toggle('no-tasks')
+      
+        let newitem = document.createElement('div')
+
+        newitem.classList.toggle('add-task')
+        let paragah = document.createElement('p')
+
+
+        paragah.innerHTML = input.value
+        console.log(paragah.innerHTML)
+        if (paragah.innerHTML == '') {
             alert("write something")
             newitem.removeChild(paragah)
-        }
-            else{
-
-        newitem.appendChild(paragah)
-        newitem.style.listStyle='none'
-
-            }
-
-        // paragah.addEventListener('click',() => {
-        //     paragah.remove()
-        // })
-        let removeitem=document.createElement('a')
-        removeitem.innerHTML=`<i class="fa fa-trash"></i>`
-        newitem.appendChild(removeitem)
-
-        let chckitem=document.createElement('a')
-        chckitem.innerHTML=`<i class="fa fa-check"></i>`
-        newitem.appendChild(chckitem)
-
-        let edit=document.createElement('a')
-        edit.innerHTML=`<i class="fa fa-edit""></i>`
-        newitem.appendChild(edit)
-
-        
-
-     
     
+        }
+        else {
+
+            newitem.appendChild(paragah)
+         
+
         
+
+        let icons = document.createElement("div");
+
+
+
+
+        let trashIcon = document.createElement('i')
+        trashIcon.innerHTML = `<i class="fa fa-trash"></i>`
+        icons.appendChild(trashIcon);
+
+
+
+        // icons.appendChild(checkIcon);
+
+        let chckitem = document.createElement('i')
+        chckitem.innerHTML = `<i class="fa fa-check"></i>`
+        icons.appendChild(chckitem);
+
+
+        let edit = document.createElement('a')
+        edit.innerHTML = `<i class="fa fa-edit""></i>`
+
+        icons.appendChild(edit);
+
+        newitem.appendChild(icons)
+
+        newitem.className = "item";
+
+
         listtask.appendChild(newitem)
-        
-        removeitem.addEventListener("click",(eo) => {
-            newitem.remove()
-           
+      
+        trashIcon.addEventListener("click", function () {
+            newitem.remove();
+
         })
 
         chckitem.addEventListener("click", (eo) => {
-if (paragah.style.textDecoration =="none"){
+            if (paragah.style.textDecoration == "none") {
 
-            paragah.style.textDecoration = "line-through";
-}else{
-    paragah.style.textDecoration ="none";
-}
+                paragah.style.textDecoration = "line-through";
+            } else {
+                paragah.style.textDecoration = "none";
+            }
         });
 
- 
 
-        edit.addEventListener("click",(eo) => {
-      
-        
-            // let save=document.createElement('a')
-            // let saveitem=edit.innerHTML=`<i class="fa fa-save"></i>`
-            // console.log(saveitem)
-            // save.innerText='save'
-            // newitem.appendChild(save)
-      
-          if(edit.innerText.toLocaleLowerCase()==edit){
-            paragah.contentEditable = true
-            paragah.style.border = "solid black";
-            // paragah.focus()
-            edit.innerText='save'
-            
-          }else{
-            paragah.contentEditable = false 
-            paragah.style.border = "none";
-            edit.innerText=edit
-          }
-            
+
+        edit.addEventListener("click", (eo) => {
+
+
+            if (edit.innerText.toLocaleLowerCase() == edit) {
+                paragah.contentEditable = true
+                paragah.style.border = "solid black";
+                edit.innerText = 'save'
+
+            } else {
+                paragah.contentEditable = false
+                paragah.style.border = "none";
+                edit.innerText = edit
+            }
+
         })
+    }
 
-       
     })
 
 
 
-    //check is tasks already exits
-    
+
+
 }
 
 
 
 add_task()
-// removetasks()
+
+
+
 
